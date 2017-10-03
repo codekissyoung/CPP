dc363c84a8dc973db6094bda2e807fb281fe7216
caokaiyan <cky951010@163.com>
2017-10-03 15:35:09 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "speak.h"
 int main( int argc , char *argv[] )
 {
-	std::cout << "hello C++" << std::endl;
+	std::cout << "hello C++ \n";
 	Speak speak;
 	speak.sayHello( " codekissyoung " );
 	return(0);
