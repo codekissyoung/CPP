828aa90a91fd8062dac905f976a5c67854ec8467
caokaiyan <cky951010@163.com>
2017-11-30 17:47:41 +0800

    使用函数

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 #include "sales_data.h"
-
-using namespace std;
 
 int main()
 {
+    using namespace std;
     struct Sales_data Book1,Book2;
+    /*
     cout << "输入Book1 : " << endl;
     cin >> Book1.bookNo >> Book1.units_sold >> Book1.revenue;
     cout << "输出Book1 : " << Book1.bookNo << Book1.units_sold << Book1.revenue << endl;
+    */
+
+    double area;
+    double side;
+    cout << "输入 area : ";
+    cin >> area;
+    side = sqrt( area );
+    cout << "sqrt( area ) = " << side << endl;
     return 0;
 }
