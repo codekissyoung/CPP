157e57fbc7e51394d718a824918245c83c623215
caokaiyan <cky951010@163.com>
2017-11-30 14:27:13 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "sales_data.h"
+using namespace std;
 int main()
 {
-    std::cout << "Enter two numbers " << std::endl;
+    cout << "Enter two numbers " << endl;
     int v1,v2;
-    std::cin >> v1 >> v2;
-    std::cout << "Num1 : " << v1 << " Num2 : " << v2 << std::endl;
+    cin >> v1 >> v2;
+    cout << "Num1 : " << v1 << " Num2 : " << v2 << endl;
     return 0;
 }
