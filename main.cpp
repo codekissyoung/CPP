6a05cc47a1dd4835ca8a3a1de6b85877265de567
caokaiyan <cky951010@163.com>
2017-12-01 14:24:16 +0800

    update

--- main.cpp
+++ main.cpp
@@ -14,15 +14,29 @@ void say_hello ( const char *str ); // 函数声明
 
 int main()
 {
-    say_hello("codekisssyoung"); // 函数调用
-    struct Sales_data Book1,Book2;
-
+    Sales_data Book1,Book2; // Sales_data 是结构体 C++ 中使用结构体声明变量 可以省略 struct
     double area;
-    double side;
+    char   name[20];
+
+    // 没啥卵用的枚举
+    enum spectrum { red, orange, yellow, green, blue, violet, indigo, ultraviolet };
+    spectrum band;
+    band = blue;
+
+    // 使用 new 来分配内存
+    int* pn = new int;
+    *pn = 10;
+    cout << "pn = " << pn << " *pn = " << *pn << endl;
+
+    cout << "输入name : ";
+    cin.getline( name, 20 ); // 读取一行 通过换行符来确定结尾 但结果不保存换行符
+    cout << name << endl;
+
+    say_hello("codekisssyoung"); // 函数调用
     cout << "输入 area : ";
     cin >> area;
-    side = sqrt( area );
-    cout << "sqrt( area ) = " << side << endl;
+    cout << "sqrt( area ) = " << sqrt( area ) << endl;
+
     return 0;
 }
 
@@ -31,6 +45,19 @@ void say_hello ( const char *str )
 {
     cout << "hello " << str << endl;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
