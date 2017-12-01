511efe413bb979869642e8fad17fa24de3c3dfc5
caokaiyan <cky951010@163.com>
2017-12-01 16:07:06 +0800

    update

--- main.cpp
+++ main.cpp
@@ -15,23 +15,40 @@ void say_hello ( const char *str ); // 函数声明
 int main()
 {
     Sales_data Book1,Book2; // Sales_data 是结构体 C++ 中使用结构体声明变量 可以省略 struct
-    double area;
-    char   name[20];
 
     // 没啥卵用的枚举
     enum spectrum { red, orange, yellow, green, blue, violet, indigo, ultraviolet };
     spectrum band;
     band = blue;
 
-    // 使用 new 来分配内存
-    int* pn = new int;
-    *pn = 10;
-    cout << "pn = " << pn << " *pn = " << *pn << endl;
-
+    char   name[20];
     cout << "输入name : ";
     cin.getline( name, 20 ); // 读取一行 通过换行符来确定结尾 但结果不保存换行符
     cout << name << endl;
 
+    // 使用 new 来分配内存
+    int* pn = new int;
+    *pn = 10;
+    cout << "pn = " << pn << " *pn = " << *pn << endl;
+    delete pn; // 释放内存
+
+    int dynamic_arr_size = 0;
+    cout << "输入想存储数组的个数: ";
+    cin >> dynamic_arr_size;
+    double* dynamic_arr = new double[ dynamic_arr_size ]; // 动态数组
+    for( int i = 0; i < dynamic_arr_size; i++ )
+    {
+        cout << "dynamic_arr[ " << i << " ] : ";
+        cin >> dynamic_arr[i];
+    }
+    for( int i = 0; i < dynamic_arr_size; i++ )
+    {
+        cout << "dynamic_arr[ " << i << " ] : " << dynamic_arr[i] << endl;
+    }
+    delete []dynamic_arr; // 释放整个动态数组内存
+
+
+    double area;
     say_hello("codekisssyoung"); // 函数调用
     cout << "输入 area : ";
     cin >> area;
