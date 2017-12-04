568ff2a355013bc53ddde27e13b37b315689ec75
caokaiyan <cky951010@163.com>
2017-12-04 18:53:54 +0800

    update

--- main.cpp
+++ main.cpp
@@ -11,41 +11,6 @@
 
 using namespace std; // 作用于当前整个文件
 
-// 使用模板
-template <typename T>
-void Swap( T &a, T &b);
-
-template <typename T>
-void Swap( T *a, T *b, int n); // 重载模板
-
-void vardump( const int arr[], int n ); // 打印一个数组
-
-int main()
-{
-    // 测试使用模板 来自动完成重载过程
-    int i = 10;
-    int j = 20;
-    cout << "i : " << i << " j : " << j << endl;
-    Swap( i, j );
-    cout << "i : " << i << " j : " << j << endl;
-
-    double di = 20.11;
-    double dj = 90.22;
-    cout << "di : " << di << " dj : " << dj << endl;
-    Swap( di, dj );
-    cout << "di : " << di << " dj : " << dj << endl;
-
-    // 重载模板
-    int d1[3] = {1,2,3};
-    int d2[3] = {6,7,9};
-
-    Swap( d1, d2, sizeof(d1) / sizeof( int ) );
-    vardump( d2 , sizeof( d2 ) / sizeof( int ) );
-
-    return 0;
-
-}
-
 template <typename T>
 void Swap( T &a, T &b)
 {/*{{{*/
@@ -76,3 +41,30 @@ void vardump( const int arr[], int n )
     }
     cout << endl;
 }/*}}}*/
+
+int main()
+{
+    // 测试使用模板 来自动完成重载过程
+    int i = 10;
+    int j = 20;
+    cout << "i : " << i << " j : " << j << endl;
+    Swap( i, j );
+    cout << "i : " << i << " j : " << j << endl;
+
+    double di = 20.11;
+    double dj = 90.22;
+    cout << "di : " << di << " dj : " << dj << endl;
+    Swap( di, dj );
+    cout << "di : " << di << " dj : " << dj << endl;
+
+    // 重载模板
+    int d1[3] = {1,2,3};
+    int d2[3] = {6,7,9};
+
+    Swap( d1, d2, sizeof(d1) / sizeof( int ) );
+    vardump( d2 , sizeof( d2 ) / sizeof( int ) );
+
+    return 0;
+
+}
+
