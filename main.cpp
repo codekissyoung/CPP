576d4da52fea9fe792e8ada57ac184f9eb5e70c3
caokaiyan <cky951010@163.com>
2017-12-07 16:39:02 +0800

    基类 与 子类

--- main.cpp
+++ main.cpp
@@ -9,16 +9,92 @@
 #include "common.h"
 #include "stock.h"
 
+class Person
+{
+    public:
+        Person();
+        ~Person();
+        void eat();
+        string name;
+        int age;
+};
+
+Person::Person()
+{
+    cout << "Person()" << endl;
+}
+
+Person::~Person()
+{
+    cout << "~Person()" <<endl;
+}
+
+void Person::eat()
+{
+    cout << "Person eat" << endl;
+}
+
+
+/*
+ * 继承方式
+ * Class A : public Class B
+ * Class A : protected Class B
+ * Class A : private B
+ * */
+
+class Worker : public Person
+{
+    public:
+        Worker();
+        ~Worker();
+        void eat();
+        void work();
+        double salary;
+};
+
+Worker::Worker()
+{
+    cout << "Worker()" << endl;
+}
+
+Worker::~Worker()
+{
+    cout << "~Worker()" << endl;
+}
+
+void Worker::eat()
+{
+    cout << "worker eat" << endl;
+}
+
+void Worker::work()
+{
+    cout << "name : " << name << " age : " << age << " salary : " << salary << endl;
+}
+
 using namespace std; // 作用于当前整个文件
 int main( int argc, char *argv[] )
 {
+    // 实例化一个子类
+    Worker *worker = new  Worker();
+    worker -> name = "codekissyoung";
+    worker -> age = 22;
+    worker -> salary = 11000.00;
+    worker -> eat();
+    worker -> Person :: eat();
+    worker -> work();
+    delete worker;
+    worker = NULL;
+
+    // 对下面这个二维数组进行排序
     char str_arr[][30] = {
         "hello",
         "jack",
         "香港",
         "澳门",
         "7呵呵呵",
-        "9fuck"
+        "9fuck",
+        "hello world"
     };
 
     int len = sizeof(str_arr) / sizeof( str_arr[0] );
@@ -69,7 +145,6 @@ int main( int argc, char *argv[] )
     ShowArray( pd, 3 );
     cout << "10 + 34.90 = " << mixed( 10, 34.90 ) << endl;
 
-
     // 股票demo
     Stock stock_cat = Stock( "NanoSmart", 20, 12.50 );
     stock_cat.show();
@@ -92,13 +167,11 @@ int main( int argc, char *argv[] )
         Stock("腾讯",120,200.89),
         Stock("阿里",60,6.22)
     };
-
     Stock* top = &mystuff[0];
     for( int i = 1; i < 4 ; i++ )
     {
         *top = top -> topval( mystuff[i] );
     }
-
     top -> show();
 
     return 0;
