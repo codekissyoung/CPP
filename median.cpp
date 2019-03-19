1c5ec1754c33b363f4b32806c03ffef7a7516815
caokaiyan <cky951010@163.com>
2019-03-19 10:51:53 +0800

    update

--- median.cpp
+++ median.cpp
@@ -4,22 +4,19 @@
 
 using namespace std;
 
-
 // 计算中值
 // 整个参数都会被复制
 double median( vector<double> vec )
-{/*{{{*/
+{
     typedef vector<double>::size_type vec_sz;
     vec_sz size = vec.size();
 
     if( size == 0 )
-    {
         throw domain_error( "median of an empty vector " );
-    }
 
     sort( vec.begin(), vec.end() );
 
     vec_sz mid = size / 2;
 
     return ( size % 2 == 0 ) ? ( vec[mid] + vec[ mid - 1 ] ) / 2 : vec[ mid ];
-}/*}}}*/
+}
