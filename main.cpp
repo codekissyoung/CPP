82b6a1de8b88aea9501b4dcdd0a31a4751222f98
caokaiyan <cky951010@163.com>
2019-04-02 15:46:46 +0800

    练习C++

--- main.cpp
+++ main.cpp
@@ -33,9 +33,34 @@ int main(int argc, char *argv[] )
 {
     try
     {
-        Screen myScreen( 5, 5, 'x' );
-        myScreen.move( 4, 0 ).set('#').display(cout);
-        return EXIT_SUCCESS;
+        cout << "学生姓名 : ";
+        string name;
+        cin >> name;
+        
+        cout << "成绩: ";
+        double m_score, f_score;
+        cin >> m_score >> f_score;
+
+        double x = 0;
+        vector<double> homework;
+        while( cin >> x )
+            homework.push_back( x );
+
+        auto count = homework.size();
+
+        sort( homework.begin(), homework.end() );
+        
+        auto mid = count / 2;
+        double median;
+        median = count % 2 == 0 ? ( homework[mid] + homework[mid-1] ) / 2 : homework[mid]; 
+
+        cout << endl
+             << "期中:" << m_score 
+             << " 期末:" << f_score 
+             << " 家庭:" << median
+             << " 总成绩:" << 0.2 * m_score + 0.4 * f_score + 0.4 * median << endl;
+ 
+        return 0;
     }
     catch( runtime_error& e )
     {
