0f17ffba43c4b34e89d193ca4591e17458790f0c
codekissyoung <cky951010@163.com>
2019-05-15 01:16:32 +0800

    添加一些 c 系统调用文件进来

--- main.cpp
+++ main.cpp
@@ -32,18 +32,15 @@
 #include <cstring>
 #include <fstream>
 
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 using namespace std;
 
 int main( int argc, char *argv[] ) {
     cout << "runing ..." << endl;
-
-    shared_ptr<string> p1; // 一个空智能指针
-
-    if( p1 && p1->empty() )
-        *p1 = "hi";
-
-    string test_str = "hello world";
-
 
     return EXIT_SUCCESS;
 }
