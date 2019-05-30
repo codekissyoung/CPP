81e7798988f25cf023ca28c4a5300afc3b381557
codekissyoung <cky951010@163.com>
2019-05-31 00:59:57 +0800

    upgrade

--- main.cpp
+++ main.cpp
@@ -54,14 +54,13 @@ typedef struct Node{
 
 typedef Node* LinkList;
 
-
 // 从 LinkList 里获取第 i 个元素的值，存入 e
-bool get_elem( LinkList L, int i, ElemType *e )
+bool get_elem( LinkList L, uint32_t i, ElemType *e )
 {
     LinkList p = L -> next; // 第一个结点
-    int j = 1;
+    uint32_t j = 1;
 
-    while( p != nullptr )
+    while( p != nullptr && j <= i )
     {
         if( j == i )
         {
@@ -76,21 +75,19 @@ bool get_elem( LinkList L, int i, ElemType *e )
     return false;
 }
 
-// 在L的第 i 个元素之前，插入新元素e
-bool insert_elem( LinkList L, int i, ElemType *e ){
-    // 等价于在 第 ( i - 1 ) 个元素后 插入 新元素 e
-    i = i - 1;
+// 在 L 的第 i 个元素后面，插入新元素 e
+bool insert_elem_after( LinkList L, uint32_t i, const ElemType e ){
 
     LinkList p = L; // 头结点
-    int j = 0;
+    uint32_t j = 0;
 
-    while( p != nullptr )
+    while( p != nullptr && j <= i )
     {
-        if( j == i )
-        {
-            auto new_elem = new Node{ *e, nullptr };
-            new_elem -> next = p -> next;
-            p -> next = new_elem;
+        if( j == i ){
+            auto new_elem = new Node{ e, nullptr };
+            new_elem -> next = p -> next;       // 新节点先指向后一个元素
+            p -> next = new_elem;               // 当前元素再指向新节点
+            return true;
         }else{
             p = p -> next;
             ++j;
@@ -100,17 +97,82 @@ bool insert_elem( LinkList L, int i, ElemType *e ){
     return false;
 }
 
+// 在 L 的第 i 个元素之前，插入新元素 e
+bool insert_elem_before( LinkList L, uint32_t i, const ElemType e ){
+    // 等价于在 第 ( i - 1 ) 个元素后 插入 新元素 e
+    return insert_elem_after( L, i - 1, e );
+}
+
+// 删除链表的 第 i 个元素，并且将其值保存在 e 中
+bool delete_elem( LinkList L, uint32_t i, ElemType *e ){
+    // 等价于 删除 第(i - 1)个元素的后一个元素
+    i = i - 1;
+
+    LinkList p = L; // 头结点
+    uint32_t j = 0;
+
+    while( p != nullptr && j <= i )
+    {
+        if( j == i ){
+            LinkList del_elm = p -> next; // 要删除的节点
+            p -> next = p->next->next;    // 当前节点指向，后面节点 的 后面节点
+            *e = del_elm -> data;         // 保存删除节点的 数据到 e
+            delete del_elm;
+            return true;
+        }else{
+            p = p -> next;
+            ++j;
+        }
+    }
+
+    return false;
+}
+
+// 清除一个链表，操作结果将 L 重置为空表
+bool clear_list( LinkList L ){
+
+    LinkList first_elm = L -> next; // 第一个元素
+    LinkList del_elem  = nullptr;
+
+    while ( first_elm != nullptr )
+    {
+        del_elem = first_elm;           // 即将删除 第一个元素
+        first_elm = first_elm -> next;  // 下一个元素 成为 第一个元素
+        delete del_elem;               // 删除第一个元素
+    }
+
+    L -> next = nullptr;
+    return true;
+}
+
 int main( int argc, char *argv[] )
 {
     LinkList students = new Node{ 0, nullptr };
 
-    ElemType a = 1009;
+    insert_elem_after( students, 0, 1099 );
+    insert_elem_before( students, 1, 2934 );
+    insert_elem_after( students, 1, 94856 );
+    insert_elem_after( students, 1, 1938954 );
+    insert_elem_after( students, 1, 4223455 );
 
-    insert_elem( students, 1, &a );
+    ElemType del_elm;
+    delete_elem( students, 1, &del_elm );
+    cout << "delete " << del_elm << endl;
 
-    ElemType b = 9912;
+    ElemType a;
+    int i = 1;
+    while ( get_elem( students, i, &a ) ){
+        cout << "a : " << a << endl;
+        ++i;
+    }
 
-    insert_elem( students, 1, &b );
+    clear_list( students );
+
+    i = 1;
+    while ( get_elem( students, i, &a ) ){
+        cout << "a : " << a << endl;
+        ++i;
+    }
 
     return EXIT_SUCCESS;
 }
