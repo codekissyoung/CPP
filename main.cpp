590bf6bc1c13d5b0cc0e0335dfc02b9b96bfc223
codekissyoung <cky951010@163.com>
2019-06-15 22:01:31 +0800

    完成psh shell

--- main.cpp
+++ main.cpp
@@ -51,11 +51,76 @@
 #include <pwd.h>
 #include <grp.h>
 #include <iomanip>
+#include <wait.h>
 
 using namespace std;
 
+char *makestring( char *buf );
+void excute( char *arglist[] );
+void parent_code( int childpid );
+
+#define MAXARGS 20
+#define ARGLEN 100
+
 int main( int argc, char *argv[] )
 {
-
+    char *arglist[MAXARGS + 1] = {};
+    int numargs = 0;
+    char argbuff[ARGLEN];
+    while ( numargs < MAXARGS ){
+        printf("Arg[%d] ", numargs);
+        fgets(argbuff, ARGLEN, stdin);
+        if(  *argbuff != '\n' ){
+            arglist[numargs++] = makestring( argbuff );
+        }else{
+            if( numargs > 0 ){
+                arglist[numargs] = nullptr;
+                excute( arglist );
+                numargs = 0;
+            }
+        }
+    }
     return 0;
+}
+
+char *makestring( char *buf ){
+    char *cp;
+    buf[strlen(buf) - 1] = '\0';
+    cp = (char *)malloc( strlen(buf) + 1 );
+    if( cp == nullptr ){
+        fprintf(stderr, "no memory");
+        exit(1);
+    }
+    strcpy( cp, buf );
+    return cp;
+}
+
+void excute( char *arglist[] ){
+    int pid = fork();
+    if( pid == -1 ){
+        perror("fork failed!");
+        exit(1);
+    }else if( pid == 0 ){
+        sleep(5);
+        execvp( arglist[0], arglist );
+        perror("execvp failed");
+        exit(1);
+    }else{
+        signal( SIGINT, SIG_IGN ); // 让父进程 忽略 Ctrl + C 信号
+        parent_code( pid );
+    }
+}
+
+void parent_code( int childpid ){
+    int child_status = 0;
+    int high_8, low_7, bit_7 = 0;
+
+    int wait_rv = wait( &child_status );
+    printf("done wating for %d. Wait returned: %d\n", childpid, wait_rv );
+
+    high_8 = child_status >> 8;
+    low_7  = child_status & 0x7F;
+    bit_7  = child_status & 0x80;
+
+    printf("status: exit = %d, sig = %d, core = %d\n", high_8, low_7, bit_7 );
 }
